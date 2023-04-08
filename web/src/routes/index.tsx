31fbc5d2f7ea4c69952535263556fde9d844252b
Nino Annighoefer <nino@ninoan.com>
2023-04-08 13:13:55 +0100

    Fetch fetch fetch

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { createQuery } from "@tanstack/solid-query";
 import { Match, Switch } from "solid-js";
 
-const fetchTodos = async () => [1, 2, 3];
+const fetchTodos = async () =>
+  fetch("http://0.0.0.0:3030/api/list").then((r) => r.json());
 
 export default function Home() {
   const query = createQuery(() => ["todos"], fetchTodos);
@@ -10,7 +11,7 @@ export default function Home() {
     <main class="text-center mx-auto text-gray-700 p-4">
       <Switch>
         <Match when={query.isError}>Error</Match>
-        <Match when={query.isSuccess}>{query.data}</Match>
+        <Match when={query.isFetched}>{JSON.stringify(query.data)}</Match>
       </Switch>
     </main>
   );
