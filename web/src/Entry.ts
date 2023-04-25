ee2912210755a3a9e6099db2e3624e4122febc5f
Nino Annighoefer <nino@ninoan.com>
2023-04-25 09:05:54 +0100

    Format everything

--- web/src/Entry.ts
+++ web/src/Entry.ts
@@ -2,10 +2,9 @@ import { z } from "zod";
 import { zodDateTime } from "./utils";
 
 export const Entry = z.object({
-  id: z.number(),
-  name: z.string(),
-  created_at: zodDateTime(),
+    id: z.number(),
+    name: z.string(),
+    created_at: zodDateTime(),
 });
 
 export type Entry = z.infer<typeof Entry>;
-
